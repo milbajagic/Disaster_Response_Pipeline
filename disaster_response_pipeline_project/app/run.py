b50569b688d119f893277a1e886f7f065d37cf84
milbajagic <43002114+milbajagic@users.noreply.github.com>
2020-05-18 14:00:21 +0200

    add second graph

--- disaster_response_pipeline_project/app/run.py
+++ disaster_response_pipeline_project/app/run.py
@@ -42,6 +42,10 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    
+    # extract category data
+    category_names = list(df.columns[4:])
+    category_counts = list(df.iloc[:,4:].sum())
 
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -61,6 +65,24 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+           'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+              },
+                'xaxis': {
+                    'title': "Category"
                 }
             }
         }
